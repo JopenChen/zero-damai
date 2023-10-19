9ee0707f4764de5c621508cee83657bbdd50f8e8
JopenChen <18318159812@163.com>
2023-10-19 15:38:54 +0800

    update
    完善服务配置

--- gateway/internal/svc/service_context.go
+++ gateway/internal/svc/service_context.go
@@ -2,14 +2,21 @@ package svc
 
 import (
 	"github.com/JopenChen/zero-damai/gateway/internal/config"
+	"github.com/JopenChen/zero-damai/service/client/service"
+	"github.com/JopenChen/zero-damai/service/client/testservice"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config         config.Config
+	ServiceRpc     service.Service
+	TestServiceRpc testservice.TestService
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:         c,
+		ServiceRpc:     service.NewService(zrpc.MustNewClient(c.ServiceRpc)),
+		TestServiceRpc: testservice.NewTestService(zrpc.MustNewClient(c.ServiceRpc)),
 	}
 }
