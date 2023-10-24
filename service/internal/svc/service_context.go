b8900c8099b95164a30c3c98fb40ee92c2e46f92
JopenChen <18318159812@163.com>
2023-10-24 11:19:51 +0800

    RPC 服务注册 model

--- service/internal/svc/service_context.go
+++ service/internal/svc/service_context.go
@@ -1,14 +1,20 @@
 package svc
 
 import (
+	"github.com/JopenChen/zero-damai/model/mysql/performance"
+	"github.com/JopenChen/zero-damai/model/mysql/performance_seat"
+	"github.com/JopenChen/zero-damai/model/mysql/performance_session"
 	"github.com/JopenChen/zero-damai/model/mysql/user"
 	"github.com/JopenChen/zero-damai/service/internal/config"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config    config.Config
-	UserModel user.UserModel
+	Config                  config.Config
+	UserModel               user.UserModel
+	PerformanceModel        performance.PerformanceModel
+	PerformanceSessionModel performance_session.PerformanceSessionModel
+	PerformanceSeatModel    performance_seat.PerformanceSeatModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -16,7 +22,10 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	mysqlConn := sqlx.NewMysql(c.Mysql.Datasource)
 
 	return &ServiceContext{
-		Config:    c,
-		UserModel: user.NewUserModel(mysqlConn),
+		Config:                  c,
+		UserModel:               user.NewUserModel(mysqlConn),
+		PerformanceModel:        performance.NewPerformanceModel(mysqlConn),
+		PerformanceSessionModel: performance_session.NewPerformanceSessionModel(mysqlConn),
+		PerformanceSeatModel:    performance_seat.NewPerformanceSeatModel(mysqlConn),
 	}
 }
